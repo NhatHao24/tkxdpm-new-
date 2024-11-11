a7717de64170aee635d9189e43c72c973325044b
NhatHao24 <nguyenhao6424@gmail.com>
2024-11-11 22:22:44 +0700

    Add files via upload

---
+++ nguyennhathao_22150607/src/main/java/com/tkxdpm/App.java
@@ -0,0 +1,13 @@
+package com.tkxdpm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
